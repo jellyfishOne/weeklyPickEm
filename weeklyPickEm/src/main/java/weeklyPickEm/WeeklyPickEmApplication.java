cd54fb03ea6efa65eb85005fe548476cd00b58ee
jellyfishOne <jrenteria.camino@gmail.com>
2021-06-30 19:46:25 -0700

    Initial commit

---
+++ weeklyPickEm/src/main/java/weeklyPickEm/WeeklyPickEmApplication.java
@@ -0,0 +1,13 @@
+package weeklyPickEm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyPickEmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyPickEmApplication.class, args);
+	}
+
+}
---
+++ weeklyPickEm/src/test/java/weeklyPickEm/WeeklyPickEmApplicationTests.java
@@ -0,0 +1,13 @@
+package weeklyPickEm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyPickEmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
