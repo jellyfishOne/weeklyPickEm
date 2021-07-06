2004c4b0577731e41d6312df143a216ff30fbdc6
jellyfishOne <jrenteria.camino@gmail.com>
2021-07-05 19:49:48 -0700

    Configure spring-restdocs to work with mockMVC

--- weeklyPickEm/src/test/java/weeklyPickEm/WeeklyPickEmApplicationTests.java
+++ weeklyPickEm/src/test/java/weeklyPickEm/WeeklyPickEmApplicationTests.java
@@ -1,24 +1,29 @@
 package weeklyPickEm;
 
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import weeklyPickEm.repositories.MatchesRepository;
 import weeklyPickEm.web.controller.PickEmController;
 import weeklyPickEm.web.model.SeasonMatchesDto;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(PickEmController.class)
 class WeeklyPickEmApplicationTests {
 	
