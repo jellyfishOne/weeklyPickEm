9bb46d58627eed6989750c6d3c388a7ce3e187bb
jellyfishOne <jrenteria.camino@gmail.com>
2021-07-07 18:14:40 -0700

    Added FieldDescriptor[] to WeeklyPiclEmApplicationTest, added index.adoc, and request-fields.snipet

--- weeklyPickEm/src/main/java/weeklyPickEm/web/controller/PickEmController.java
+++ weeklyPickEm/src/main/java/weeklyPickEm/web/controller/PickEmController.java
@@ -1,18 +1,28 @@
 package weeklyPickEm.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import weeklyPickEm.web.model.SeasonMatchesDto;
+import weeklyPickEm.web.services.PickEmServices;
 
 @RestController
 @RequestMapping("api/v1/pickem")
 public class PickEmController {
 	
+	@Autowired
+	PickEmServices pickEmService;
+	
 	@GetMapping("/season-matches")
-	public void getSeasonMatches() {
-		
+	public ResponseEntity getSeasonMatches() {
+		List<SeasonMatchesDto> seasonMatches = pickEmService.getSeasonMatches();
+		return ResponseEntity.status(HttpStatus.OK).body(seasonMatches);
 	}
 
 }
--- weeklyPickEm/src/test/java/weeklyPickEm/WeeklyPickEmApplicationTests.java
+++ weeklyPickEm/src/test/java/weeklyPickEm/WeeklyPickEmApplicationTests.java
@@ -3,7 +3,7 @@ package weeklyPickEm;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -20,11 +20,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import weeklyPickEm.repositories.MatchesRepository;
 import weeklyPickEm.web.controller.PickEmController;
 import weeklyPickEm.web.model.SeasonMatchesDto;
+import weeklyPickEm.web.services.PickEmServices;
 
 @ExtendWith(RestDocumentationExtension.class)
 @AutoConfigureRestDocs
@@ -37,6 +42,9 @@ class WeeklyPickEmApplicationTests {
 	@MockBean
 	MatchesRepository matchesRepository;
 	
+	@MockBean 
+	PickEmServices services;
+	
 	@Mock 
 	SeasonMatchesDto seasonMatches = SeasonMatchesDto.builder().build();
 	
@@ -44,17 +52,21 @@ class WeeklyPickEmApplicationTests {
 	void getSeasonMatches() throws Exception{
 		List<SeasonMatchesDto> seasonMatchesList = new ArrayList<SeasonMatchesDto>();
 		seasonMatchesList.add(seasonMatches);
-		seasonMatchesList.add(seasonMatches);
 		
 		given(matchesRepository.findAll()).willReturn(seasonMatchesList);
+		
+		FieldDescriptor[] seasonMatches = new FieldDescriptor[] {
+				fieldWithPath("id").description("Season Id").type("String").optional(),
+				fieldWithPath("userName").description("User Name").type("String").optional() };
 		
 		mockMvc.perform(get("/api/v1/pickem/season-matches")
 						.accept(MediaType.APPLICATION_JSON))
 						.andExpect(status().isOk())
-						.andDo(document("season-matches",
+						.andDo(document("v1/get-season-matches",
 								responseFields(
-										fieldWithPath("id").description("Season matches Id")
-										
-										)));
+										fieldWithPath("[]").description("ArrayList of matches"))
+											.andWithPrefix("[].", seasonMatches)
+										)).andDo(MockMvcResultHandlers.print());
+		
 	}
 }
